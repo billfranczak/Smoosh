8c869cee00c17a33c11869b807d587b20448eb49
Ian Woksey <meat4every1@gmail.com>
2017-03-06 19:04:18 -0500

    -- Application.targetFrameRate Hopefully fixes build issues?!?!

---
+++ smoosh/Assets/scripts/SceneControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControl : MonoBehaviour {
+    public int frameRate = 60;
+	// Use this for initialization
+	void Awake () {
+        Application.targetFrameRate = frameRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
